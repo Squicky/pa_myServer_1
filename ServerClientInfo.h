3831753f259fa15661b5ec5cf778a45ef30ecf5f
user <user@uservpc>
2014-08-26 17:20:52 +0200

    mit IPs

--- ServerClientInfo.h
+++ ServerClientInfo.h
@@ -5,12 +5,26 @@
  * Created on 19. August 2014, 10:40
  */
 
+
+/*
+ * eth0: 192.168.120.233
+ * eth1: 192.168.220.235
+ * 
+ * iptables -t nat -A POSTROUTING -o eth1 -d 192.168.220.236 -s 192.168.220.235 -j SNAT --to-source 192.168.120.234
+
+iptables -t nat -A PREROUTING -i eth0 -s 192.168.120.234 -d 192.168.220.236 -j DNAT --to-destination 192.168.120.233
+
+iptables -t nat -A POSTROUTING -o eth0 -d 192.168.120.234 -s 192.168.120.233 -j SNAT --to-source 192.168.220.236
+
+iptables -t nat -A PREROUTING -i eth1 -s 192.168.220.236 -d 192.168.120.234 -j DNAT --to-destination 192.168.220.235 
+ */
+
 #ifndef SERVERCLIENTINFO_H
 #define	SERVERCLIENTINFO_H
 
 #include <time.h>
 
-#define SERVER_IP "192.168.2.231"
+#define SERVER_IP "192.168.120.233"
 
 
 struct init_info_client_to_server {
